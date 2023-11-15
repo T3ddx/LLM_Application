9cbc57b9817425e1b38428b729c83abd50b952dd
Theodor Dumitru <teddy.dumitru@yahoo.com>
2023-11-14 20:25:51 -0500

    Can generate shit

--- BigramLanguageModel.py
+++ BigramLanguageModel.py
@@ -27,7 +27,7 @@ class BigramLanguageModel(nn.Module):
 
     #takes in a training data and the targets
     #returns the logits and loss of comparing the logits to the targets
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         #takes in 4 (batch size, B) tensors with 8 (block size, T) chars in them
             #chars come as ints
         #idx is (B, T) so (4, 8)
@@ -35,19 +35,24 @@ class BigramLanguageModel(nn.Module):
         #each tensor will contain another T (8) tensors that include the embeddings of each char in T
         #so the logits is just a tensor that includes all embeddings for the 4 batches of training data
         logits = self.table(idx)
-        B, T, C = logits.shape
 
-        #must reshape the tensor since loss takes in logits of (C) or (N,C)
-        #streches out the tensor
-        #now is a tensor of length B*T (32) which represents the total amount of training characters we send in
-        #The elements of this tensor are the embedding associated with the character
-        logits = logits.view(B*T, C)
-        #targets must match the shape of logits
-        targets = targets.view(B*T)
-        #loss measures the error in the training data compared to the validating data
-        #does this by doing a shit ton of math 
-        #returns a tensor that has the loss
-        loss = F.cross_entropy(logits, targets)
+        #targets do not have to be specified
+        #if there are none, loss is 0
+        if targets is not None:
+            B, T, C = logits.shape
+            #must reshape the tensor since loss takes in logits of (C) or (N,C)
+            #streches out the tensor
+            #now is a tensor of length B*T (32) which represents the total amount of training characters we send in
+            #The elements of this tensor are the embedding associated with the character
+            logits = logits.view(B*T, C)
+            #targets must match the shape of logits
+            targets = targets.view(B*T)
+            #loss measures the error in the training data compared to the validating data
+            #does this by doing a shit ton of math 
+            #returns a tensor that has the loss
+            loss = F.cross_entropy(logits, targets)
+        else:
+            loss = 0
 
         return logits, loss
     
@@ -58,9 +63,33 @@ class BigramLanguageModel(nn.Module):
         #remember _ means we don't care about that value
             #from haskell!!!!
         for _ in range(max_new_token):
-            pass
+            #the size of the logits will be 1x_x61
+            #where _ is the number of token that the for loop is on
+            #the B is only 1 since we are only looking at batch
+            logits, loss = self(idx)
+
+            #will only focus on the latest char added
+            #will become 1x61 vector
+            logits = logits[:,-1]
+            
+            #makes a vector of size 61
+            #contains the probability of each elements based off the logits
+            #every probability will add up to 1
+            soft = F.softmax(logits, dim=-1)
+
+            #picks an index from the vector given
+            #will pick based off of the probability given in the vector
+            #picks num_samples number of samples
+            next = torch.multinomial(soft, num_samples=1)
+            
+            #adds to tensors together
+            #dim=1 means that it will add it to the next dimension
+            idx = torch.concat((idx, next), dim=1)
+        return idx
 
     
 m = BigramLanguageModel(t.vocab_size)
 logits, loss = m(t.x,t.y)
-logits = logits[:,-1, :]
+
+print(t.decode(m.generate(torch.zeros((1,1), dtype=torch.long),1000)[0].tolist()))
+#print(F.softmax(logits, dim=0))
