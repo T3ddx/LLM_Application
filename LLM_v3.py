9067941b91016144c3da46e05b65658e82386d57
PaulCIV <plclark@syr.edu>
2024-03-12 02:49:40 -0400

    time for teddy the weirdo

--- LLM_v3.py
+++ LLM_v3.py
@@ -1,46 +1,34 @@
 from langchain_openai import ChatOpenAI
 from langchain.memory import ChatMessageHistory
 from langchain_core.messages import AIMessage, HumanMessage, BaseMessage
-from langchain_core.load import Serializable
-import os
 import constants
+import magic
+from langchain.memory import ChatMessageHistory
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.document_loaders.parsers import BS4HTMLParser, PDFMinerParser
+from langchain.document_loaders.parsers.generic import MimeTypeBasedParser
+from langchain.document_loaders.parsers.txt import TextParser
+from langchain_community.document_loaders import Blob
 
-#os.environ['OPENAI_API_KEY'] = constants.API_KEY
 
 chat = ChatOpenAI(model="gpt-3.5-turbo-1106", openai_api_key = constants.API_KEY)
 
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+parse = {
+    "application/pdf": PDFMinerParser(),
+    "text/plain": TextParser(),
+}
 
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         (
-#             "system",
-#             "You are a helpful assistant. Answer all questions to the best of your ability.",
-#         ),
-#         MessagesPlaceholder(variable_name="messages"),
-#     ]
-# )
-
-# chain = prompt | chat
-
-# chain.invoke(
-#     {
-#         "messages": [
-#             HumanMessage(
-#                 content="Translate this sentence from English to French: I love programming."
-#             ),
-#             AIMessage(content="J'adore la programmation."),
-#             HumanMessage(content="What did you just say?"),
-#         ],
-#     }
-# )
-
+messages = []
 
 
 while True:
     human = input("Does Teddy have a Gyat: \n")
-    print(chat.generate([[HumanMessage(human)]]).generations.pop().pop().text)
+    messages.append(HumanMessage(human))
+    response = chat.generate([messages]).generations.pop().pop().text
+    messages.append(AIMessage(response))
+    print(response)
+    
 
 
 
